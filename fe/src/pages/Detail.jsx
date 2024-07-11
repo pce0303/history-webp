ae7ac43de45c944429d2eb64daafea0b44ec2c99
pce0303 <ivychoen@gmail.com>
2024-07-12 08:56:10 +0900

    asdf

--- fe/src/pages/Detail.jsx
+++ fe/src/pages/Detail.jsx
@@ -1,7 +1,11 @@
 export default function Detail() {
   return(
     <div id="wrapper4">
-      
+      <h1 className="detail-title"></h1>
+      <h3 className="subTitle"></h3>
+      <p className="detail-value" id="process"></p>
+      <p className="detail-value" id="limit"></p>
+      <p className="detail-value" id="result"></p>
     </div>
   )
 }
--- fe/src/pages/Main.jsx
+++ fe/src/pages/Main.jsx
@@ -3,7 +3,7 @@ import '../styles/main.css';
 export default function Main() {
   return(
     <div id="wrapper">
-      <h1>title</h1>
+      <h1>역사 속에서 정보윤리 찾기</h1>
       <a href="/era/three"><div className="era"><p>삼국</p></div></a>
       <a href="/era/korea"><div className="era"><p>고려</p></div></a>
       <a href="/era/josun"><div className="era"><p>조선</p></div></a>
