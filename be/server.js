c9e34178e6340018b0bae305289d74f9b262ba73
pce0303 <ivychoen@gmail.com>
2024-07-14 22:16:37 +0900

    T^T

--- be/server.js
+++ be/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import conn from './database/db.js';
+import eraRouter from './routes/era.js'
+import detailRouter from './routes/detail.js';
 import path from 'path';
 const __dirname = import.meta.dirname;
 
@@ -7,9 +9,11 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../fe/dist')));
+app.use('/era/*', eraRouter);
+app.use('/title/*', detailRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../fe/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../fe/dist/index.html'));
 })
 
 app.get('/', (req, res) => {
